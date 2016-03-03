d70bb5697ab95f60ecba36a1e0179b8ff287c9b1
Sheng <sheng.di@citrix.com>
2016-03-02 19:53:11 -0800

    aboutme wip

--- public/scripts/app/content/aboutme.js
+++ public/scripts/app/content/aboutme.js
@@ -10,12 +10,18 @@ define(function (require, exports, module) {
         // This is (one) convention used to manage the scope of 'this', common in Knockout examples:
         var self = this;
 
+        var $window = $(window),
+            win_height_padded = $window.height() * 1.1,
+            isTouch = Modernizr.touch;
+
+        var index=0;
+
         var feed = new Instafeed({
             get: "user",
             userId: 471220422,
             accessToken: '471220422.f517982.3d5b579ed5464add809c2cb8fcb759ae',
             clientId: 'f51798263d50429e99f8a3e92f6932b4',
-            limit: 7,
+            limit: 6,
             after: function () {
                 var images = $("#instafeed").find('a');
                 $.each(images, function (index, image) {
@@ -31,8 +37,61 @@ define(function (require, exports, module) {
             template: '<a href="{{link}}" target="_blank"><img src="{{image}}" /></a>'
         });
 
+        if (isTouch) {
+            $('.revealOnScroll').addClass('animated');
+        }
+
+        $window.on('scroll', revealOnScroll);
+
+        function revealOnScroll() {
+            var scrolled = $window.scrollTop(),
+                win_height_padded = $window.height() * 1.1;
+
+            // Showed...
+            $(".revealOnScroll:not(.animated)").each(function () {
+                var $this = $(this),
+                    offsetTop = $this.offset().top;
+
+                if (scrolled + win_height_padded > offsetTop) {
+                    if ($this.data('timeout')) {
+                        window.setTimeout(function () {
+                            $this.addClass('animated ' + $this.data('animation'));
+                        }, parseInt($this.data('timeout'), 10));
+                    } else {
+                        $this.addClass('animated ' + $this.data('animation'));
+                    }
+                }
+            });
+            // Hidden...
+            $(".revealOnScroll.animated").each(function (index) {
+                var $this = $(this),
+                    offsetTop = $this.offset().top;
+                if (scrolled + win_height_padded < offsetTop) {
+                    $(this).removeClass('animated fadeInUp flipInX lightSpeedIn')
+                }
+            });
+        }
+
         self.activate = function(){
             feed.run();
+            revealOnScroll();
+            $(document).scroll(function(){
+                var top = $('.technical').height()-$(window).scrollTop();
+                if(top<-3600){
+                    if(index==0){
+                        $('.chart').easyPieChart({
+                            barColor: '#e9a1b9',
+                            size: 152,
+                            lineWidth: 10,
+                            onStep: function(from, to, percent) {
+                                $(this.el).find('.percent').text(Math.round(percent));
+                            }
+                        });
+
+                    }
+                    index++;
+                }
+            });
         }
     }
 
